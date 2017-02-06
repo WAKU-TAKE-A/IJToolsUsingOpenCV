964bdb884c0262f528fd17263a6e21b94bbc135f
WAKU-TAKE-A <takehito.nishida@gmail.com>
2017-02-06 23:07:10 +0900

    update

--- OCV_GaussianBlur.java
+++ OCV_GaussianBlur.java
@@ -9,7 +9,6 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.core.Point;
 import org.opencv.core.Size;
 
 /*
