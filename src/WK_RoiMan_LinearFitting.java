50f10b01b87ebcbcda23a62304bda2e9eaf4b244
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2025-12-30 01:22:58 +0900

    update

--- src/WK_RoiMan_LinearFitting.java
+++ src/WK_RoiMan_LinearFitting.java
@@ -11,6 +11,7 @@ import ij.plugin.frame.RoiManager;
 import ij.process.ImageProcessor;
 import java.awt.Frame;
 import java.awt.Rectangle;
+import java.awt.Polygon;
 import java.util.ArrayList;
 import org.opencv.core.Point;
 
@@ -76,6 +77,7 @@ public class WK_RoiMan_LinearFitting implements ExtendedPlugInFilter {
 
             if(enRefData) {
                 rsTbl.reset();
+                num_run = 0;
             }
 
             return FLAGS;
@@ -94,22 +96,15 @@ public class WK_RoiMan_LinearFitting implements ExtendedPlugInFilter {
             return DONE;
         }
         else {
-            // get the ImagePlus
             impSrc = imp;
-
-            // get the ResultsTable
             rsTbl = getResultsTable(false);
-
-            // get the ROI Manager
             roiMan = getRoiManager(false, true);
-            int num_roi = roiMan.getCount();
-
-            if(num_roi == 0) {
+            
+            if(roiMan.getCount() == 0) {
                 IJ.error("ROI is vacant.");
                 return DONE;
             }
 
-            // get the selected rois
             selectedIndexes = roiMan.getSelectedIndexes();
 
             if(selectedIndexes == null || selectedIndexes.length == 0) {
@@ -122,39 +117,29 @@ public class WK_RoiMan_LinearFitting implements ExtendedPlugInFilter {
 
     @Override
     public void run(ImageProcessor ip) {
-        int num_slctd = selectedIndexes.length;
-
-        boolean all_eq_x = true;
-        boolean all_eq_y = true;
-        int num_all = 0;
-        double ini_x = 0;
-        double ini_y = 0;
-        double sxy = 0;
-        double sx = 0;
-        double sy = 0;
-        double sxx = 0;
         ArrayList<Point> lstPt = new ArrayList<Point>();
 
-        for(int i = 0; i < num_slctd; i++) {
+        for(int i = 0; i < selectedIndexes.length; i++) {
             Roi roi = roiMan.getRoi(selectedIndexes[i]);
             getCoordinates(roi, lstPt);
         }
 
-        ini_x = lstPt.get(0).x;
-        ini_y = lstPt.get(0).y;
-        num_all = lstPt.size();
+        int num_all = lstPt.size();
+
+        if (num_all < 2) {
+            IJ.error("At least 2 points are required.");
+            return;
+        }
+
+        double sx = 0, sy = 0, sxx = 0, sxy = 0;
+        double ini_x = lstPt.get(0).x;
+        boolean all_eq_x = true;
 
         for(int i = 0; i < num_all; i++) {
             double x = lstPt.get(i).x;
             double y = lstPt.get(i).y;
 
-            if(ini_x != x) {
-                all_eq_x = false;
-            }
-
-            if(ini_y != y) {
-                all_eq_y = false;
-            }
+            if(x != ini_x) all_eq_x = false;
 
             sx += x;
             sy += y;
@@ -162,142 +147,82 @@ public class WK_RoiMan_LinearFitting implements ExtendedPlugInFilter {
             sxy += x * y;
         }
 
-        double a = (double)num_all * (double)sxx - (double)sx * (double)sx;
+        double a = (double)num_all * sxx - sx * sx;
         double slope = 0;
         double intercept = 0;
 
-        if(all_eq_x && all_eq_y) {
-            IJ.error("only one point");
-            return;
-        }
-        else if(all_eq_x && !all_eq_y) {
-            slope = 0.0 / 0.0;
-            intercept = 0.0 / 0.0;
-        }
-        else if(!all_eq_x && all_eq_y) {
-            slope = 0;
-            intercept = ini_y;
-        }
-        else {
-            slope = ((double)num_all * (double)sxy - (double)sx * (double)sy) / a;
-            intercept = ((double)sxx * (double)sy - (double)sxy * (double)sx) / a;
+        if (Math.abs(a) < 1e-12 || all_eq_x) {
+            slope = Double.POSITIVE_INFINITY;
+            intercept = Double.NaN;
+        } else {
+            slope = ((double)num_all * sxy - sx * sy) / a;
+            intercept = (sxx * sy - sxy * sx) / a;
         }
 
         rsTbl.incrementCounter();
-        rsTbl.addValue("Slope", String.valueOf(slope));
-        rsTbl.addValue("Intercept", String.valueOf(intercept));
+        rsTbl.addValue("Slope", slope);
+        rsTbl.addValue("Intercept", intercept);
         rsTbl.show("Results");
 
         if(enAddRoi) {
-            double x1 = 0.0;
-            double y1 = 0.0;
-            double x2 = 0.0;
-            double y2 = 0.0;
+            double x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+            double w = (double)impSrc.getWidth();
+            double h = (double)impSrc.getHeight();
 
-            if(all_eq_x && !all_eq_y) {
-                double h = (double)(impSrc.getHeight());
-                x1 = (double)ini_x;
-                y1 = 0.0;
-                x2 = x1;
-                y2 = h + (double)num_run;
-            }
-            else if(!all_eq_x && all_eq_y) {
-                double w = (double)(impSrc.getWidth());
-                x1 = 0.0;
-                y1 = (double)ini_y;
-                x2 = w + (double)num_run;
-                y2 = y1;
-            }
-            else {
-                if(-1 <= slope && slope <= 1) {
-                    double w = (double)(impSrc.getWidth());
-                    x1 = 0.0;
-                    y1 = slope * x1 + intercept;
-                    x2 = w + (double)num_run;
-                    y2 = slope * x2 + intercept;
-                }
-                else {
-                    double h = (double)(impSrc.getHeight());
-                    y1 = 0.0;
-                    x1 = (y1 - intercept) / slope;
-                    y2 = h + (double)num_run;
-                    x2 = (y2 - intercept) / slope;
-                }
+            if (Double.isInfinite(slope)) {
+                x1 = ini_x; y1 = 0;
+                x2 = ini_x; y2 = h;
+            } else {
+                x1 = 0;
+                y1 = intercept;
+                x2 = w;
+                y2 = slope * w + intercept;
             }
 
             Line ln = new Line(x1, y1, x2, y2);
+            int currentCount = roiMan.getCount();
             roiMan.addRoi(ln);
+            roiMan.rename(currentCount, "Fit_Line_" + num_run);
             num_run++;
         }
     }
 
-    /**
-     * get the ResultsTable or create a new ResultsTable
-     * @param enReset reset or not
-     * @return ResultsTable
-     */
     private ResultsTable getResultsTable(boolean enReset) {
         ResultsTable rt = ResultsTable.getResultsTable();
-
         if(rt == null || rt.getCounter() == 0) {
             rt = new ResultsTable();
         }
-
         if(enReset) {
             rt.reset();
         }
-
         rt.show("Results");
-
         return rt;
     }
 
-    /**
-     * get the RoiManager or create a new RoiManager
-     * @param enReset reset or not
-     * @param enShowNone show none or not
-     * @return RoiManager
-     */
     private RoiManager getRoiManager(boolean enReset, boolean enShowNone) {
         Frame frame = WindowManager.getFrame("ROI Manager");
-        RoiManager rm = null;
-
-        if(frame == null) {
-            rm = new RoiManager();
-            rm.setVisible(true);
-        }
-        else {
-            rm = (RoiManager)frame;
-        }
-
-        if(enReset) {
-            rm.reset();
-        }
-
-        if(enShowNone) {
-            rm.runCommand("Show None");
-        }
-
+        RoiManager rm = (frame == null) ? new RoiManager() : (RoiManager)frame;
+        if(frame == null) rm.setVisible(true);
+        if(enReset) rm.reset();
+        if(enShowNone) rm.runCommand("Show None");
         return rm;
     }
 
-    /**
-     * get the coordinates of the roi(ref:XYCoordinates.saveSelectionCoordinates())
-     * @param roi
-     * @param lstPt
-     */
     private void getCoordinates(Roi roi, ArrayList<Point> lstPt) {
-        ImageProcessor mask = roi.getMask();
-        Rectangle r = roi.getBounds();
-        int pos_x = 0;
-        int pos_y = 0;
-
-        for(int y = 0; y < r.height; y++) {
-            for(int x = 0; x < r.width; x++) {
-                if(mask == null || mask.getPixel(x, y) != 0) {
-                    pos_x = r.x + x;
-                    pos_y = r.y + y;
-                    lstPt.add(new Point(pos_x, pos_y));
+        if (roi.getType() >= Roi.POLYLINE && roi.getType() <= Roi.FREELINE || roi.getType() == Roi.LINE) {
+            // Use Polygon for better compatibility with older ImageJ versions
+            Polygon p = roi.getPolygon();
+            for (int i = 0; i < p.npoints; i++) {
+                lstPt.add(new Point(p.xpoints[i], p.ypoints[i]));
+            }
+        } else {
+            ImageProcessor mask = roi.getMask();
+            Rectangle r = roi.getBounds();
+            for(int y = 0; y < r.height; y++) {
+                for(int x = 0; x < r.width; x++) {
+                    if(mask == null || mask.getPixel(x, y) != 0) {
+                        lstPt.add(new Point(r.x + x, r.y + y));
+                    }
                 }
             }
         }
